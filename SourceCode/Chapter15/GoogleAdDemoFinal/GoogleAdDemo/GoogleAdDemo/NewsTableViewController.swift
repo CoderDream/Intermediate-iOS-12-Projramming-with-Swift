92c54104107d9e3cfe5c96da906a2e45139c6f08
CoderDream <coderdream@gmail.com>
2019-07-01 22:25:46 +0800

    1501 init 20190701

---
+++ SourceCode/Chapter15/GoogleAdDemoFinal/GoogleAdDemo/GoogleAdDemo/NewsTableViewController.swift
@@ -0,0 +1,148 @@
+//
+//  NewsTableViewController.swift
+//  GoogleAdDemo
+//
+//  Created by Simon Ng on 29/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class NewsTableViewController: UITableViewController {
+
+    lazy var adBannerView: GADBannerView = {
+        let adBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
+//        adBannerView.adUnitID = "ca-app-pub-8501671653071605/1974659335"
+        adBannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        adBannerView.delegate = self
+        adBannerView.rootViewController = self
+        
+        return adBannerView
+    }()
+    
+    var interstitial: GADInterstitial?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+        
+        let adRequest = GADRequest()
+        adRequest.testDevices = [ kGADSimulatorID, "2077ef9a63d2b398840261c8221a0c9b" ]
+        adBannerView.load(GADRequest())
+        
+        interstitial = createAndLoadInterstitial()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows
+        return 4
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsTableViewCell
+        
+        // Configure the cell...
+        if indexPath.row == 0 {
+            cell.postImageView.image = UIImage(named: "red-lights-lisbon")
+            cell.postTitle.text = "Red Lights, Lisbon"
+            cell.postAuthor.text = "TOM EVERSLEY (@tomeversley)"
+            cell.authorImageView.image = UIImage(named: "appcoda-fav-logo")
+            
+        } else if indexPath.row == 1 {
+            cell.postImageView.image = UIImage(named: "val-throrens-france")
+            cell.postTitle.text = "Val Thorens, France"
+            cell.postAuthor.text = "BARA ART (bara-art.com)"
+            cell.authorImageView.image = UIImage(named: "appcoda-fav-logo")
+            
+        } else if indexPath.row == 2 {
+            cell.postImageView.image = UIImage(named: "summer-beach-huts")
+            cell.postTitle.text = "Summer Beach Huts, England"
+            cell.postAuthor.text = "TOM EVERSLEY (@tomeversley)"
+            cell.authorImageView.image = UIImage(named: "appcoda-fav-logo")
+            
+        } else {
+            cell.postImageView.image = UIImage(named: "taxis-nyc")
+            cell.postTitle.text = "Taxis, NYC"
+            cell.postAuthor.text = "TOM EVERSLEY (@tomeversley)"
+            cell.authorImageView.image = UIImage(named: "appcoda-fav-logo")
+            
+        }
+        
+        return cell
+    }
+    
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return adBannerView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return adBannerView.frame.height
+    }
+    
+    // MARK: - Interstitial Ads
+    
+    private func createAndLoadInterstitial() -> GADInterstitial? {
+        interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        
+        guard let interstitial = interstitial else {
+            return nil
+        }
+        
+        let request = GADRequest()
+        // Remove the following line before you upload the app
+        request.testDevices = [ kGADSimulatorID ]
+        interstitial.load(request)
+        interstitial.delegate = self
+        
+        return interstitial
+    }
+}
+
+extension NewsTableViewController: GADBannerViewDelegate {
+    
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        print("Banner loaded successfully")
+        
+        // Reposition the banner ad to create a slide down effect
+        let translateTransform = CGAffineTransform(translationX: 0, y: -bannerView.bounds.size.height)
+        bannerView.transform = translateTransform
+        
+        UIView.animate(withDuration: 0.5) {
+            bannerView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        print("Fail to receive ads")
+        print(error)
+    }
+}
+
+extension NewsTableViewController: GADInterstitialDelegate {
+    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
+        print("Interstitial loaded successfully")
+        ad.present(fromRootViewController: self)
+    }
+    
+    func interstitialDidFail(toPresentScreen ad: GADInterstitial) {
+        print("Fail to receive interstitial")
+    }
+}
